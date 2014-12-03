41ffa285117a7dd00bc5384ed56963d77c2340cf
pgoudy <Pete.goudy@gmail.com>
2014-12-02 20:55:21 -0700

    Py script now uses rudimentary MySQL to store info & contains a simple parity check

--- list.py
+++ list.py
@@ -3,51 +3,73 @@
 
 import os
 import os.path
+import MySQLdb
 
 music = []
 picture = []
 video = []
 inaction = []
+locations = {}
 
-def mediaSort(directory, fdirect): #sorts files in given directory into 4 distinct lists. Gives full location if fdirect is 1
+db = MySQLdb.connect(host="", user = "", passwd = "", db = "") #fill in w/ proper values
+cur = db.cursor()
+
+def mediaSort(directory): #sorts files in given directory into 4 distinct lists. Gives full location if fdirect is 1
     print "Working in:" + directory
     for x in os.listdir(directory):
         longx = directory+"/"+x
-        if fdirect == True:
-            x = longx
         extension = os.path.splitext(x)[1] #scrapes the extension for file x
-        if os.path.isfile(longx) == 0: #if a folder is found, run mediaSort on said folder
-            mediaSort(longx, fdirect)
+        if os.path.isfile(longx) == 0 and x != ".git": #if a folder is found, run mediaSort on said folder
+            mediaSort(longx)
             continue
         if extension in ['.jpg', '.png', '.gif']: #list so can easily be added to
             picture.append(x)
+            locations[x] = longx;
             continue
         if extension in ['.mov', '.ogv', '.wmv']:
             video.append(x)
+            locations[x] = longx;
             continue
         if extension in ['.mp3', '.wav', '.ogg']:
             music.append(x)
+            locations[x] = longx;
             continue
         if extension in ['.txt', '.py']: #easy way to disregard certain extensions
             continue
         else:
             inaction.append(x) #in case we want to list files we do nothing with
-        
-def createFile(inlist, listname): #creates a .txt file from a list, given said list and filename
-    outfile = open(listname, 'w')
-    inlist.sort() #might be unneeded
-    for x in inlist:
-        outfile.write(x + "\n")
-    print "%s files in " %len(inlist) + listname
 
-def test (): #personal test code. Runs mediasort in directory of list.py, and creates text files. Will be removed eventually.
-    mediaSort(os.getcwd(), 0)
-    createFile(picture, "pictures.txt")
-    createFile(video, "videos.txt")
-    createFile(music, "music.txt")
-    if len(inaction) != 0: #will probably move to a function later
-        print "No action taken on:"
-        for x in inaction:
-            print x
+    
+def fillTable(medialist, listname):
+    cur.execute("truncate %s" % listname)
+    for media in medialist:
+        cur.execute("INSERT INTO %s (name, location) VALUES (%%s, %%s)" % listname, (media, locations[media]))
+
+def parityCheck(medialist, listname): #returns 1 if values in a list are the same as db 
+    cur.execute("SELECT Name from %s" %listname)
+    parity = []
+    for row in cur:
+        parity.append(row[0])
+    for media in medialist:
+        if media in parity:
+            continue
+        else:
+            return 0
+    for media in parity:
+        if media in medialist:
+            continue
+        else:
+            return 0
+    return 1
+    
+    
+
+def test (): #personal test code. Runs mediasort in directory of list.py, and fills out a table. Will be removed eventually.
+    mediaSort(os.getcwd())
+    print parityCheck(picture, "pictures")
+    #fillTable(picture, "pictures")
+    #fillTable(music, "music")
+    #fillTable(video, "videos")
+    db.close()
     
 test()

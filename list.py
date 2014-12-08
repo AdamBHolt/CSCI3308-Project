49c542c0cde28f68eef447d0814edee616eadde3
Peter Goudy <pete.goudy@gmail.com>
2014-12-08 14:28:49 -0700

    main function added, majority of functionality should be there.

--- list.py
+++ list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# Overall, pretty rough. What it does is lists files in three distinct categories (music, pics, video) and writes the names of said files into three files to use later. Can be modified, but is a good starting point.
 
 import os
 import os.path
@@ -15,14 +14,13 @@ db = MySQLdb.connect(host="", user = "", passwd = "", db = "") #fill in w/ prope
 cur = db.cursor()
 
 def mediaSort(directory): #sorts files in given directory into 4 distinct lists. Gives full location if fdirect is 1
-    print "Working in:" + directory
     for x in os.listdir(directory):
         longx = directory+"/"+x
         extension = os.path.splitext(x)[1] #scrapes the extension for file x
-        if os.path.isfile(longx) == 0 and x != ".git": #if a folder is found, run mediaSort on said folder
+        if os.path.isfile(longx) == 0 and x != ".git" and x != "home": #if a folder is found, run mediaSort on said folder
             mediaSort(longx)
             continue
-        if extension in ['.jpg', '.png', '.gif']: #list so can easily be added to
+        if extension in ['.jpg', '.png', '.gif']:
             picture.append(x)
             locations[x] = longx;
             continue
@@ -41,11 +39,10 @@ def mediaSort(directory): #sorts files in given directory into 4 distinct lists.
 
     
 def fillTable(medialist, listname):
-    cur.execute("truncate %s" % listname)
     for media in medialist:
         cur.execute("INSERT INTO %s (name, location) VALUES (%%s, %%s)" % listname, (media, locations[media]))
 
-def parityCheck(medialist, listname): #returns 1 if values in a list are the same as db 
+def parityCheck(medialist, listname): #returns 1 if values in a list are the same as db
     cur.execute("SELECT Name from %s" %listname)
     parity = []
     for row in cur:
@@ -61,15 +58,20 @@ def parityCheck(medialist, listname): #returns 1 if values in a list are the sam
         else:
             return 0
     return 1
-    
-    
 
-def test (): #personal test code. Runs mediasort in directory of list.py, and fills out a table. Will be removed eventually.
+def main ():
     mediaSort(os.getcwd())
-    print parityCheck(picture, "pictures")
-    #fillTable(picture, "pictures")
-    #fillTable(music, "music")
-    #fillTable(video, "videos")
+    if parityCheck(picture, "pictures") == 0:
+        cur.execute("TRUNCATE pictures")
+        fillTable(picture, "pictures")
+    if parityCheck(music, "music") == 0:
+        cur.execute("TRUNCATE music")
+        fillTable(music, "music")
+    if parityCheck(video, "videos") == 0:
+        cur.execute("TRUNCATE videos")
+        fillTable(video, "videos")
     db.close()
+        
     
-test()
+        
+    
